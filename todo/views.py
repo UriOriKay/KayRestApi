960403d983017df7981e222d7e98178a7725a3b3
UriOriKay <127621006+UriOriKay@users.noreply.github.com>
2024-09-21 08:26:15 +0200

    first working version

---
+++ todo/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import TodoSerializer
+from .models import Todo
+
+
+
+# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all().order_by("-created_at")
+    serializer_class = TodoSerializer
+    permission_classes = [] # permissions.IsAuthenticated
