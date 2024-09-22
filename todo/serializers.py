36a1efd3c657ec434c6d2194dcd151f624c7d67a
UriOriKay <127621006+UriOriKay@users.noreply.github.com>
2024-09-22 08:06:34 +0200

    for debug review

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,8 +2,14 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Todo
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username', 'email']
+
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Todo
         fields = ['id', 'title', 'description', 'created_at']
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
+from django.core import serializers
+from django.http import HttpResponse
 
 
 
@@ -11,3 +13,12 @@ class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all().order_by("-created_at")
     serializer_class = TodoSerializer
     permission_classes = [] # permissions.IsAuthenticated
+
+    def create(self, request):
+        todo = Todo.objects.create(
+            title=request.data.get("title", ""),
+            description=request.data.get("description", ""),
+            user = self.request.user,
+        )
+        serialized_obj = serializers.serialize('json', [todo])
+        return HttpResponse(serialized_obj, content_type="application/json")
